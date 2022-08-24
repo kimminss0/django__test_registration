b4015981938654a92ab969dcf462500e13668543
blabla7127 <blabla7127@outlook.kr>
2022-08-24 15:38:54 +0900

    created views

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    pass
+
+def register(request):
+    pass
+
+def reg_redirect(request):
+    pass
+
+def logout(request):
+    pass
+
