213258f898447e7673b6b20fc686ce3eb0f26d57
blabla7127 <blabla7127@outlook.kr>
2022-08-27 05:41:20 +0900

    (test)

--- calendarApp/calSerializers.py
+++ calendarApp/calSerializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
-from .models import Cal
+from .models import Cal, Small
+
+class SmallSerializer(serializers.ModelSerializer):
+    s1 = serializers.CharField()
+    s2 = serializers.CharField()
+    # class Meta:
+    #     model = Small
+    #     fields = '__all__'
 
 class CalSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cal
-        fields = '__all__'
+    s = SmallSerializer()
+    var1 = serializers.CharField()
+    var2 = serializers.CharField()
+    var3 = serializers.CharField()
+    # class Meta:
+    #     model = Cal
+    #     fields = '__all__'
--- calendarApp/models.py
+++ calendarApp/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Small(models.Model):
+    s1 = models.TextField()
+    s2 = models.TextField()
+
 class Cal(models.Model):
+    s = models.Field()
     var1 = models.TextField()
     var2 = models.TextField()
     var3 = models.TextField()
